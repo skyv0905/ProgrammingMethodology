dcbb34f20cae67242abc3a6b7ae0e581ccbbb5a8
seotbeo <yoonv72@naver.com>
2023-11-12 15:50:52 +0900

    init

---
+++ Project/Constants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define WINDOW_X 300
+#define WINDOW_Y 300
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 640
+
+#define boundaryX (WINDOW_WIDTH)/2
+#define boundaryY (WINDOW_HEIGHT)/2
---
+++ Project/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include "GL/freeglut.h"
+#include "Constants.h"
+
+using namespace std;
+
+clock_t start_t = clock();
+clock_t end_t;
+
+void initialize() {
+
+}
+
+
+void idle() {
+	end_t = clock();
+	if ((float)(end_t - start_t) > 1000 / 30.0f) {
+
+		start_t = end_t;
+	}
+
+	glutPostRedisplay();
+}
+
+void display() {
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-boundaryX, boundaryX, -boundaryY, boundaryY, -100.0, 100.0);
+	//gluOrtho2D(-boundaryX, boundaryX, -boundaryY, boundaryY);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glutWireCube(50.0f);
+
+	glutSwapBuffers();
+}
+
+int main(int argc, char** argv) {
+	// init GLUT and create Window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowPosition(WINDOW_X, WINDOW_Y);
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	glutCreateWindow("Bubble Bobble");
+	initialize();
+
+	// register callbacks
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+
+	// enter GLUT event processing cycle
+	glutMainLoop();
+
+	return 0;
+}
